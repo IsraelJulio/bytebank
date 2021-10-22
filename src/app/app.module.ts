952143a16cc7d1186e7a0162a922ce22463d539d
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-10-22 08:51:09 -0300

    criação do componente extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { Rodape } from './rodape/rodape.component';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NovaTransferenciaComponent,
-    Rodape
+    Rodape,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
+
+
 export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino : number;
+
   transferir() {
-    console.log('solicitada...');
+
+    const valoresParaEmitir = {valor: this.valor, destino: this.destino};
+
+    this.aoTransferir.emit(valoresParaEmitir);
+
+    this.LimparCampos();
   }
+  LimparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  };
 }
