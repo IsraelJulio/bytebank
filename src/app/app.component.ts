66c241123ec76c582d2debeac9fb39690cba1ceb
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-11-30 19:32:10 -0300

    Criação do Serviço de Tranferencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
-  id = 1;
   title = 'bytebank';
-  Transferencia: any[] = [];
-
 
   tranferirParaAppComponent($event: any) {
-
-    const addId = {...$event,id:this.id++};
-
-    this.Transferencia.push(addId);
+    // const addId = {...$event,id:this.id++};
+    // this.Transferencia.push(addId);
+    this.transferenciaService.adicionar($event);
   }
+  constructor(private transferenciaService: TransferenciaService) {}
 }
-
